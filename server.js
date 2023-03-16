fe6fd2a8e13650fbeeba38777d3f340c8a0398b3
Dewei99 <d.zhou1999@gmail.com>
2023-03-16 22:36:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const app = express();
 app.use(express.json());
 
 // static files
-app.use("/app",express.static(path.resolve(__dirname,"app")));
-
+//app.use("/app",express.static(path.resolve(__dirname,"app")));
+app.use("/app",express.static(path.join(__dirname, "/app")));
 
 //Conexion a base de datos
 const uri=`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ftd5eru.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;//url de conexion
