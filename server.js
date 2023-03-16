cbc6b78f8faed7458cb8ae5e182440f65f1e92fa
Dewei99 <d.zhou1999@gmail.com>
2023-03-16 20:29:58 +0100

    cambios

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.json());
 // static files
 app.use("/app",express.static(path.resolve(__dirname,"app")));
 
+
 //Conexion a base de datos
 const uri=`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ftd5eru.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;//url de conexion
 
